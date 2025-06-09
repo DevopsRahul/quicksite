425aa28a656338f05d165b01d9007f7f3380be8b
DevopsRahul <116802122+DevopsRahul@users.noreply.github.com>
2025-06-09 21:07:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello from React + Vite!</h1>
+      <h1>Hello from React + Vite! Made New Version</h1>
     </div>
   );
 }
